c0f01754079d0952941454407f9a96c6890d7220
baena@LCC.UMA.ES <baena@LCC.UMA.ES@2dacbcc4-1239-11df-8689-7786f20063ad>
2009-05-18 14:36:18 +0000

    * Nueva opción -l: Lista mensajes en orden. * fallos en gestión de directorios en MessageReader.

--- gnusmail/src/gnusmail/MessageReader.java
+++ gnusmail/src/gnusmail/MessageReader.java
@@ -3,6 +3,7 @@ package gnusmail;
 import gnusmail.core.cnx.Connection;
 
 
+import java.util.Date;
 import java.util.Iterator;
 import java.util.TreeSet;
 
@@ -14,9 +15,16 @@ public class MessageReader implements Iterable<Message>{
 	private class ComparableMessage implements Comparable<ComparableMessage>{
 
 		private Message message;
+		private Date date;
 
 		public ComparableMessage(Message msg) {
 			this.message = msg;
+			try {
+				this.date = msg.getReceivedDate();
+			} catch (MessagingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 
 		public Message getMessage() {
@@ -25,14 +33,7 @@ public class MessageReader implements Iterable<Message>{
 
 		@Override
 		public int compareTo(ComparableMessage o) {
-			int value =  0;
-			try {
-				value = this.message.getReceivedDate().compareTo(o.message.getReceivedDate());
-			} catch (MessagingException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			return value;
+			return this.date.compareTo(o.date);
 		}
 		
 		
@@ -44,19 +45,33 @@ public class MessageReader implements Iterable<Message>{
 		TreeSet<ComparableMessage> message_list;
 		public MessageReaderIterator(Connection connection) {
 			message_list = new TreeSet<ComparableMessage>();
-			Folder[] folders;
+			long total_msgs = 0;
+			Folder[] folders = null;
 			try {
 				folders = connection.getCarpetas();
-				for (Folder folder : folders) {
-					Message msg = folder.getMessage(1); // this is the first message
-					ComparableMessage comparableMsg = new ComparableMessage(msg);
-					message_list.add(comparableMsg);
-				}
 			} catch (MessagingException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-
+			for (Folder folder : folders) {
+				try {
+				if (!folder.isOpen()) {
+						folder.open(javax.mail.Folder.READ_ONLY);
+				}
+				System.out.println(folder.getFullName());
+				int msg_count = folder.getMessageCount();
+				total_msgs += msg_count;
+				if (msg_count<=0) {
+					continue;
+				}
+				Message msg = folder.getMessage(1); // this is the first message
+				ComparableMessage comparableMsg = new ComparableMessage(msg);
+				message_list.add(comparableMsg);
+				folder.close(false);
+				} catch (MessagingException e) {
+					e.printStackTrace();
+				}
+			}
+			System.out.println("Total: " + total_msgs);
 		}
 		@Override
 		public boolean hasNext() {
@@ -70,10 +85,15 @@ public class MessageReader implements Iterable<Message>{
 			Folder folder = msg.getFolder();
 			int number = msg.getMessageNumber();
 			try {
+				if (!folder.isOpen()) {
+					folder.open(javax.mail.Folder.READ_ONLY);
+				}
 				if (number < folder.getMessageCount()) {
+					
 					Message nextMsg = folder.getMessage(number+1);
 					ComparableMessage nextComparableMsg = new ComparableMessage(nextMsg);
 					message_list.add(nextComparableMsg);
+					folder.close(false);
 				}
 			} catch (MessagingException e) {
 				e.printStackTrace();
