058234b87a9aeba4c43415ab90ebe3638ad3ff2f
jmcarmona@LCC.UMA.ES <jmcarmona@LCC.UMA.ES@2dacbcc4-1239-11df-8689-7786f20063ad>
2009-05-21 11:23:09 +0000

    capital letters proportion

---
+++ trunk/gnusmail/src/gnusmail/filters/CapitalLettersProportion.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gnusmail.filters;
+
+import gnusmail.core.cnx.MensajeInfo;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.mail.MessagingException;
+
+/**
+ *
+ * @author jmcarmona
+ */
+public class CapitalLettersProportion extends Filter {
+
+    @Override
+    public String getName() {
+        return "Capital letters proportion";
+    }
+
+    @Override
+    public String applyTo(MensajeInfo mess) {
+        int capitals = 0;
+        int total = 1;
+        try {
+            String body = mess.getBody();
+            total = body.length();
+            int index = 0;
+            while (index < body.length()) {
+                char letter = body.charAt(index);
+                if (letter >= 'A' && letter <= 'Z') {
+                    capitals++;
+                }
+            }
+        } catch (MessagingException ex) {
+            Logger.getLogger(CapitalLettersProportion.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(CapitalLettersProportion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        double proporcion = capitals / total;
+        return "" + proporcion;
+    }
+
+}
