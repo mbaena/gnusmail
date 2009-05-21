82db19144542162e946e8c48bfab44f4fbe1b33e
jmcarmona@LCC.UMA.ES <jmcarmona@LCC.UMA.ES@2dacbcc4-1239-11df-8689-7786f20063ad>
2009-05-21 14:31:24 +0000

    Herramientas de lenguaje

---
+++ trunk/gnusmail/src/gnusmail/Languages/Language.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gnusmail.Languages;
+
+/**
+ * This is an enumeration of the currently supported languages
+ * @author jmcarmona
+ */
+public enum Language {
+    SPANISH(0),
+    ENGLISH(1);
+
+
+    int codigo;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+
+
+    private Language(int codigo) {
+        this.codigo = codigo;
+
+    }
+
+
+}
