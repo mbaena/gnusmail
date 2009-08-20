ff186898b366ea54a99443f2e573a814e43592b1
jmcarmona@LCC.UMA.ES <jmcarmona@LCC.UMA.ES@2dacbcc4-1239-11df-8689-7786f20063ad>
2009-08-20 09:48:41 +0000

    Nuevos filtros implementados

---
+++ trunk/gnusmail/src/gnusmail/filters/DomainOfSender.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gnusmail.filters;
+
+import gnusmail.core.cnx.MessageInfo;
+import java.util.StringTokenizer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.mail.MessagingException;
+
+/**
+ *
+ * @author jmcarmona
+ */
+public class DomainOfSender extends Filter {
+
+    @Override
+    public String getName() {
+       return "Domain of Sender";
+    }
+
+    @Override
+    public String applyTo(MessageInfo mess) {
+        String res = "";
+        try {
+            StringTokenizer st = new StringTokenizer(mess.getFrom(), "@");
+            while (st.hasMoreTokens()) {
+                res = st.nextToken();
+            }
+        } catch (MessagingException ex) {
+            Logger.getLogger(DomainOfSender.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return res;
+    }
+
+}
