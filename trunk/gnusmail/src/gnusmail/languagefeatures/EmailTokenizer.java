49361cf559bf8f1bd18539793862f4b20d71fd8e
jmcarmona@LCC.UMA.ES <jmcarmona@LCC.UMA.ES@2dacbcc4-1239-11df-8689-7786f20063ad>
2009-08-20 11:32:57 +0000

    Nuevos filtros implementados

--- trunk/gnusmail/src/gnusmail/languagefeatures/EmailTokenizer.java
+++ trunk/gnusmail/src/gnusmail/languagefeatures/EmailTokenizer.java
@@ -2,9 +2,9 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package gnusmail.languagefeatures;
 
+import gnusmail.Languages.Language;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.StringTokenizer;
@@ -14,20 +14,31 @@ import java.util.StringTokenizer;
  * @author jmcarmona
  */
 public class EmailTokenizer {
-   String body;
-   List<Token> tokens;
-   public final static String patronToken = " \t\n\r\f.,;:?¿!¡\"()'=[]{}/<>-*0123456789ªº%&*@_|’";
 
+    String body;
+    List<Token> tokens;
+    Language lang = null;
+    public final static String tokenPattern = " \t\n\r\f.,;:?¿!¡\"()'=[]{}/<>-*0123456789ªº%&*@_|’\\#";
 
     public EmailTokenizer(String emailBody) {
         this.body = emailBody;
         tokens = new LinkedList<Token>();
     }
 
-    public  List<Token> tokenize() {
-        StringTokenizer st = new StringTokenizer(body, patronToken);
+    public EmailTokenizer(String emailBody, Language lang) {
+        this.body = emailBody;
+        tokens = new LinkedList<Token>();
+        this.lang = lang;
+    }
+
+    public List<Token> tokenize() {
+        StringTokenizer st = new StringTokenizer(body, tokenPattern);
         while (st.hasMoreElements()) {
-            tokens.add(new Token(st.nextToken()));
+            Token token = new Token(st.nextToken());
+            if (lang != null) {
+                token.setLanguage(lang);
+            }
+            tokens.add(token);
         }
         return tokens;
     }
