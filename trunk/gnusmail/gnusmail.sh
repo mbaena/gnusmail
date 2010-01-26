4a896163d7a9fe2d9912e5a337a6cfa41bc7f510
mbaena@lcc.uma.es <mbaena@lcc.uma.es@2dacbcc4-1239-11df-8689-7786f20063ad>
2010-01-26 22:16:05 +0000

    * gnusmail.sh improvements

--- trunk/gnusmail/gnusmail.sh
+++ trunk/gnusmail/gnusmail.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
 gnusmail_path=${0%/*}
+if [[ $gnusmail_path == $0 ]]
+then
+gnusmail_path="."
+fi
+
+echo $gnusmail_path
 java -javaagent:${gnusmail_path}/lib/sizeofag.jar -jar -Xmx5G ${gnusmail_path}/gnusmail.jar
