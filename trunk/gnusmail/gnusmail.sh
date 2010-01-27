7e852fdcb6124edec81e3ae0530aa1f465f3b814
jmcarmona@lcc.uma.es <jmcarmona@lcc.uma.es@2dacbcc4-1239-11df-8689-7786f20063ad>
2010-01-27 12:48:31 +0000

    Bash script arrreglado

--- trunk/gnusmail/gnusmail.sh
+++ trunk/gnusmail/gnusmail.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 gnusmail_path=${0%/*}
+
 if [[ $gnusmail_path == $0 ]]
 then
 gnusmail_path="."
 fi
 
-echo $gnusmail_path
-java -javaagent:${gnusmail_path}/lib/sizeofag.jar -jar -Xmx5G ${gnusmail_path}/gnusmail.jar
+java -javaagent:${gnusmail_path}/lib/sizeofag.jar -jar -Xmx5G ${gnusmail_path}/gnusmail.jar $*
