7bffce72d643c440da5c23f9b5b37fdaf2ee8bcf
jmcarmona@lcc.uma.es <jmcarmona@lcc.uma.es@2dacbcc4-1239-11df-8689-7786f20063ad>
2010-02-02 13:14:13 +0000

    Launch weka

--- gnusmail/experimentation/launchWeka.py
+++ gnusmail/experimentation/launchWeka.py
@@ -2,40 +2,34 @@
 """
 - En el directorio donde se ejecute este script, debe haber un acceso a weka (un vínculo, p.e.)
 - En ~.gnusmail debe haber un acceso, llamdo maildir, a la carpeta raiz del dataset de enron (ln -s)
+- Se supone que GNUSmailGoogle.jar esta en dist/, si no, crear vinculo o cambiar ruta (createDataSet y evaluateMOA)
 """
-
+import commands, sys
 import os
+import os.path
+import re
+from threadpool import *
 
-def createLink(author):
-	order = """
-rm ~/.gnusmail/maildirln ; ln -s ~/.gnusmail/maildir/%s ~/.gnusmail/maildirln
-	""" % (author)
-	print(order)
-	os.system(order)
+_GNUSMAIL_PATH=os.path.join("..", "dist")
+_GNUSMAIL_SH=os.path.join(_GNUSMAIL_PATH, "gnusmail.sh")
+_WEKA_JAR=os.path.join(_GNUSMAIL_PATH, "lib", "weka.jar")
+_MAILDIR_PATH=os.path.join("dataset","maildir")
+_OUTPUT_PATH="outputweka"
 
 def createFilteringString(selector, nwords,author):
         output = "dataset" + selector + author + str(nwords) + ".arff"
         return """
-java -cp weka.jar weka.filters.supervised.attribute.AttributeSelection -E \"weka.attributeSelection.%s \" -S "weka.attributeSelection.Ranker -T -3.0E-4 -N %s" -c \"first" -i ~/.gnusmail/dataset.arff  %s
-        """ % (selector, nwords, output), output
-
-def executeOrder(order):
-        os.system(order)
+java -cp %s weka.filters.supervised.attribute.AttributeSelection -E \"weka.attributeSelection.%s \" -S "weka.attributeSelection.Ranker -T -3.0E-4 -N %s" -c \"first" -i ~/.gnusmail/dataset.arff -o %s
+        """ % (_WEKA_JAR, selector, nwords, output), output
 
 def getAvailableSelectors():
-        return ['GainRatioAttributeEval', 'ChiSquaredAttributeEval', 'ReliefFAttributeEval', 'InfoGainAttributeEval', 'OneRAttributeEval']
+        return ['GainRatioAttributeEval', 'ChiSquaredAttributeEval', 'ReliefFAttributeEval', 'InfoGainAttributeEval', 'OneRAttributeEval'][:1]
 
-def extractWordsFromArff(file):
-        f = open(file)
-        lines = [line for line in f.readlines() if line.startswith('@attribute')]
-        words = [line.split()[1] for line in lines]
-        f.close()
-        wordsfile = file.replace('arff','list')
-        f = open(wordsfile,"w")
-        for word in words:
-		if word[0].islower():
-			f.write(word + '\n')
-        f.close()
+"""
+dataset.arff will contain the attributes of @author
+"""
+def createDataSet(maildir, datasetfile):
+	os.system('java -jar -Xmx5G %s -z%s -b%s' % (_GNUSMAIL_JAR, maildir, datasetfile))
 
 
 def createWordlists(author):
@@ -50,29 +44,19 @@ def createWordlists(author):
 			os.remove(file)
 			print ('done')
 
-def evaluate(author,numWords,selector):
-	wordsFile = 'dataset' + selector + str(numWords) + author + '.list'
-	order = """
-cp %s ~/.gnusmail/wordlist.data
-	""" % (wordsFile)
-	print(order)
-	os.system(order)
-	createLink(author)
-	os.system('rm ~/.gnusmail/atributos.csv')
-	os.system('rm ~/.gnusmail/dataset.arff')
-	os.system('rm ~/.gnusmail/model.bin')
-	os.system('java -jar -Xmx5G dist/GNUSMailGoogle.jar -z -b')
-	os.system('java -jar -Xmx5G dist/GNUSMailGoogle.jar -z -e -i')
-	newTasas = 'tasasWeka' + author + selector + str(numWords)
-	order = """
-mv tases %s
-	""" % (newTasas)
-	os.system(order)
-	newDS = "dataset" + author + selector + str(numWords) + ".arff"
-	os.system("mv ~/.gnusmail/dataset.arff " + newDS)
+def evaluateWeka(author, alg, output):
+    maildir = os.path.join(_MAILDIR_PATH, author)
+    task = """bash %s -z%s -i%s -e --weka-classifier=%s > %s.out""" % (_GNUSMAIL_SH, maildir, output, alg, output+"salidaporpantalla") #moaClassifier
+    print task
+    commands.getoutput("bash %s" % (task))
+	#os.system('java -javaagent:%s -jar -Xmx5G %s -z -m --moa-classifier="%s"' % (_SIZEOFAG_JAR, _GNUSMAIL_JAR, alg)) #moaClassifier
+
 
 def getAuthors():
 	return ['kitchen-l', 'lokay-m','sanders-r','beck-s','williams-w3', 'farmer-d', 'kaminski-v']
+
+def getWekaAlgorithms():
+	return ["weka.classifiers.bayes.NaiveBayesUpdateable"]
 
 def createWordlistsForEveryAuthor():
 	for author in getAuthors():
@@ -81,13 +65,43 @@ def createWordlistsForEveryAuthor():
 		print('---------------')
 		createWordlists(author)
 
+def imprimirgraficas(graficas):
+    for (key, value) in graficas.iteritems():
+        filePng = "grafica_" + key + ".png"
+        sentencia = "plot "
+        for gr in value:
+            if not os.path.exists(output_file):
+                continue
+            sentencia += '"' + gr + '" w l' 
+            if gr <> value[-1]:
+                sentencia += ', '
+        print sentencia
+        sentgnuplot = "echo 'set terminal png; set output \"" + filePng +  "\"; " + sentencia +  "' | gnuplot"
+        os.system(sentgnuplot)
+
 """""""""
 MAIN SECTION
 """""""""
 
 
 #createWordlistsForEveryAuthor()
+pool = ThreadPool(3)
+if not os.path.exists(_OUTPUT_PATH):
+    os.mkdir(_OUTPUT_PATH)
+graficas = {}
 for author in getAuthors():
-	for numWords in ['200','300','400']:
-		for method in getAvailableSelectors():
-			evaluate(author,numWords,method)
+    if not os.path.exists(os.path.join(_MAILDIR_PATH, author)):
+        maildir = os.path.join(_MAILDIR_PATH, author)
+        print "PATH NOT FOUND! %s " % (maildir)
+        continue
+    graficas[author] = []
+    for alg in getWekaAlgorithms():
+        output_file = os.path.join(_OUTPUT_PATH, 'rates' + author + alg.replace(" ","").replace("(","").replace(")",""))
+        if os.path.exists(output_file):
+            continue
+        print output_file
+        pool.queueTask(evaluateWeka, (author, alg, output_file))
+        graficas[author].append(output_file)
+pool.joinAll()
+imprimirgraficas(graficas)
+
