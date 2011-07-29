e75968a4baf9b4052463f7597d63f92e8446178a
jmcarmona.lcc.uma.es <jmcarmona.lcc.uma.es@2dacbcc4-1239-11df-8689-7786f20063ad>
2011-07-29 09:58:18 +0000

    remove

--- gnusmail/src/gnusmail/datasource/mailconnection/DataSource.java
+++
@@ -1,9 +0,0 @@
-package gnusmail.datasource;
-
-public abstract class DataSource {
-  public static int EMAIL_FROM_IMAPSERVER = 0;
-  public static int EMAIL_FROM_FILESYSTEM = 1;
-  
-  
-  public abstract DocumentReader getDocumentReader();
-}
